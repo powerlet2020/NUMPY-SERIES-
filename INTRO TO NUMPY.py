49f10bfc4e5df1fbe42f733b92e8abb0d59e0ad1
powerlet2020 <129383921+powerlet2020@users.noreply.github.com>
2024-07-08 18:07:37 +1200

    Add files via upload
    first commit

---
+++ INTRO TO NUMPY.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+x = np.array([1,2,3])
+print(x)
+
+
+# In[8]:
+
+
+y = np.array([[1,2,3],[4,5,6]])
+print(y)
+
+z = np.array([[10,20,20],[30,40,50],[60,70,80]])
+print(z)
+
+
+# In[12]:
+
+
+a = np.array([[[1,2,3],[2,4,5]],[[2,4,6],[3,5,6]]])
+print(a)
+
+
+# In[13]:
+
+
+print(x.ndim)
+print(y.ndim)
+print(a.ndim)
+
+
+# In[16]:
+
+
+print(type(a))
+
+
+# In[ ]:
+
+
+
+
